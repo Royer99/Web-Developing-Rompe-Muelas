0dd7860a65a768a907b7f15453c77481c1aed562
MartinAntonio123 <a01701167@itesm.mx>
2018-04-27 11:57:12 -0500

    anadir evaluacion de la trivia

--- JS/trivia.js
+++ JS/trivia.js
@@ -1,7 +1,42 @@
 $(document).ready(function(){
-    $('form').click(funcVerificar);
-    //$('input[name=p1]:checked').val();
+    $('div.shadowTrivia').hover(function(){
+        division = $(this);
+    });
+
+     $('input').click(function(){
+        valor = $(this).val();
+        funcVerificar();
+    });   
 });
 function funcVerificar(){
-    alert($(this).val());
+    division.removeClass('shadowTrivia');
+    division.removeClass('shadowTriviaRigth');
+    division.removeClass('shadowTriviaWrong');
+    if(valor == 1){
+        division.addClass('shadowTriviaRigth');
+    }
+    else{
+        division.addClass('shadowTriviaWrong');
+    }
+    suma = $('.shadowTriviaRigth').length + $('.shadowTriviaWrong').length;
+    if(suma == 10){
+        if($('.shadowTriviaRigth').length == 10){
+            alert('100% mexicano');
+        }
+        else if($('.shadowTriviaRigth').length > 8 ){
+            alert('bien hecho');
+        }
+        else if($('.shadowTriviaRigth').length > 7){
+            alert('puedes mejorar');
+        }
+        else if($('.shadowTriviaRigth').length > 6){
+            alert('panzaso');
+        }
+        else{
+            alert('sigue practicando');
+        }
+    }
 }
+var division;
+var valor;
+var suma;
